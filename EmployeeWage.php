61c244f45988e9d029bf6d51bbb27fb2b64cebd8
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-06-24 22:27:43 +0530

    Added: Calculate Daily Employee Wage

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -2,6 +2,8 @@
     class EmpWage{
         //variable
         private $check;
+        private $wagePerHour = 20;
+        private $fullDayHour = 8;
 
         //Function to display welcome message
         public function welcome(){
@@ -10,21 +12,31 @@
 
         //Function to generate randon number between 1 to 2
          public function getNumber(){
-            return $this->check = rand(1, 2);
+            $this->check = rand(1, 2);
         }
 
         //Function to check employee is present or absent
         public function attendance(){
             if($this->check == 1){
-                echo "Employee is present";
+                echo "Employee is present \n";
             }
             else{
-                echo "Employee is absent";
+                echo "Employee is absent \n";
             }
         }
+
+        //Function to calculate daily employee wage
+        public function getDailyWage(){
+            if($this->check == 1){
+                $DailyWage = $this->wagePerHour * $this->fullDayHour;
+                echo "Daily Employee Wage :" . $DailyWage;
+            }   
+        }
     }
+    //Object
     $wage = new EmpWage();
     $wage->welcome();
     $wage->getNumber();
     $wage->attendance();
+    $wage->getDailyWage();
 ?>
